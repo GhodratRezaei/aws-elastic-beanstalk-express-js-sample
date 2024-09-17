a8e0037b1bd355b0fe76ea1542614a01a656a86a
GhodratRezaei <ghodratrezaei1996@gmail.com>
2024-09-17 14:21:17 +0200

    end-to-end pipeLine

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! got changed at 11:28 AM 9/16/2024'));
+app.get('/', (req, res) => res.send('Full Test end-to-end PipeLine from source to the deployment.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
